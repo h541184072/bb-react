b37e12e027ba0989ffb63227f045aae73b58d345
huangzhibo <huangzhibo@hfjy.com>
2018-11-05 17:31:59 +0800

    router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader'
+import Routes from './router'
+import store from './public_store'
 
-import App from './App'
+import { LocaleProvider } from 'antd'
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
+import 'moment/locale/zh-cn'
 
 render(
-  <App/>,
+  <LocaleProvider locale={zh_CN}>
+    <AppContainer>
+      <Provider store={store}>
+        <Routes store={store}/>
+      </Provider>
+    </AppContainer>
+  </LocaleProvider>,
   document.getElementById('root')
 )
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 // import Loadable from './loadable'
-// import Login from '../components/pages/Login'
-import Home from '@/view/home'
-import Detail from '@/view/detail'
+import App from '../App'
+import Login from '@/view/Login'
 import NotFound from '@/view/Exception/404.js'
-
-// import App from '../App'
 
 export default function Page() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <Redirect to="/home"/>}/>
-        <Route exact path="/home" component={Home}/>
-        <Route path="/detail" component={Detail}/>
+        <Route exact path="/" render={() => <Redirect to="/app"/>}/>
+        <Route exact path="/app" component={App}/>
+        <Route path="/login" component={Login}/>
         <Route path="/404" component={NotFound}/>
-        {/*    <Route path="/login" component={Login}/>*/}
         <Route component={NotFound}/>
       </Switch>
     </Router>
